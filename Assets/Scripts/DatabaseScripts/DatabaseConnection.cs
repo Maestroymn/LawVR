132b7c3d4392816d23fcf0002016d53e0dea9d9e
Deniz Bayan <denizbayan4@gmail.com>
2021-03-30 23:20:27 +0300

    Database Connection query handling
    Now we are connected to aws database and we can handle sql queries

--- Assets/Scripts/DatabaseScripts/DatabaseConnection.cs
+++ Assets/Scripts/DatabaseScripts/DatabaseConnection.cs
@@ -7,24 +7,53 @@ namespace DatabaseScripts
     public class DatabaseConnection : MonoBehaviour
     {
         public bool EditDatabaseSettings = false;
-        [ConditionalShowInInspector("EditDatabaseSettings",true)] public string Server="127.0.0.1";
+        [ConditionalShowInInspector("EditDatabaseSettings",true)] public string Server= "lawvrdatabase.cou5tvcgh993.us-east-2.rds.amazonaws.com";
         [ConditionalShowInInspector("EditDatabaseSettings",true)] public int Port=5432;
-        [ConditionalShowInInspector("EditDatabaseSettings",true)] public string DatabaseName="test";
-        [ConditionalShowInInspector("EditDatabaseSettings",true)] public string UserID="postgres";
-        [ConditionalShowInInspector("EditDatabaseSettings",true)] public string Password="postgres";
+        [ConditionalShowInInspector("EditDatabaseSettings",true)] public string DatabaseName= "postgres";
+        [ConditionalShowInInspector("EditDatabaseSettings",true)] public string UserID= "postgres";
+        [ConditionalShowInInspector("EditDatabaseSettings",true)] public string Password= "postgres";
         // Start is called before the first frame update
         void Start()
         {
-            NpgsqlConnection baglanti = new NpgsqlConnection($"Server={Server}; Port={Port}; Database={DatabaseName}; User Id={UserID}; Password={Password}; Timeout=15;");
+            
+            NpgsqlConnection baglanti = new NpgsqlConnection($"Server={Server}; Port={Port}; Database={DatabaseName}; User Id={UserID}; Password={Password}; Timeout=45;");
             baglanti.Open();
-            Debug.Log("selam");
-            string sorgu = "insert into newtable(username) values ('at123')";
+            var cmd = new NpgsqlCommand();
+            cmd.Connection = baglanti;
 
-            var cmd = new NpgsqlCommand() ;
-
+            /*************************************************************************************
+            
+            Non-query executions like Create,Alter,Drop,Insert,Update,Delete vs works with ExecuteNonQuery.
+            
+            *************************************************************************************/
+            /*string sorgu = "insert into users(user_id,name,password) values(1234,'kaan','12345') ";
+            
             cmd.Connection = baglanti;
             cmd.CommandText = sorgu;
-            cmd.ExecuteNonQuery();
+            cmd.ExecuteNonQuery();*/
+
+
+            /*************************************************************************************
+
+            Query executions with return like select will work with ExecuteReader .
+
+            *************************************************************************************/
+            cmd.CommandText = "SELECT * FROM users";
+
+            NpgsqlDataReader tables = cmd.ExecuteReader();
+
+            //print rows
+            while(tables.Read())
+            {
+                string rowResult="";
+                for(int i = 0; i < tables.FieldCount; i++)
+                {
+                    rowResult += tables[i] + "\t";
+                }
+                Debug.Log(rowResult);
+            }
+                
+            
         }
     }
 }
